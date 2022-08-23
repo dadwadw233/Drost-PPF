c8745b6df394d2777208de15db4e4c6c88788e8b
yyh <913217005@qq.com>
2022-08-23 15:22:09 +0800

    完成PPF online基本功能

--- PPFRegistration.cpp
+++ PPFRegistration.cpp
@@ -4,4 +4,49 @@
 
 #include "PPFRegistration.h"
 
-namespace PPF {}  // namespace PPF
+namespace PPF {
+Eigen::Affine3f PPFRegistration::getFinalTransformation() {
+  return this->finalTransformation;
+}
+void PPFRegistration::setSceneReferencePointSamplingRate(
+    const float& scene_reference_point_sampling_rate) {
+  this->scene_reference_point_sampling_rate =
+      scene_reference_point_sampling_rate;
+}
+void PPFRegistration::setPositionClusteringThreshold(
+    const float& clustering_position_diff_threshold) {
+  this->clustering_position_diff_threshold = clustering_position_diff_threshold;
+}
+void PPFRegistration::setRotationClusteringThreshold(
+    const float& clustering_rotation_diff_threshold) {
+  this->clustering_rotation_diff_threshold = clustering_rotation_diff_threshold;
+}
+void PPFRegistration::setDiscretizationSteps(
+    const float& angle_discretization_step,
+    const float& distance_discretization_step) {
+  this->angle_discretization_step = angle_discretization_step;
+  this->distance_discretization_step = distance_discretization_step;
+}
+
+void PPFRegistration::setDobj(const float& data) { this->d_obj = data; }
+
+void PPFRegistration::setSearchMap(
+    const Hash::HashMap_<Hash::HashKey, Hash::HashData, Hash::hash_cal>::Ptr&
+        searchMap) {
+  this->searchMap = searchMap;
+}
+void PPFRegistration::setTransMap(
+    const Hash::HashMap_<Hash::Trans_key, Hash::Trans_data,
+                         Hash::Tran_cal>::Ptr& Trans) {
+  this->model_trans = Trans;
+}
+void PPFRegistration::setInputSource(
+    const pcl::PointCloud<pcl::PointNormal>::Ptr& cloud) {
+  this->model_cloud_with_normal = cloud;
+}
+void PPFRegistration::setInputTarget(
+    const pcl::PointCloud<pcl::PointNormal>::Ptr& cloud) {
+  this->scene_cloud_with_normal = cloud;
+}
+void PPFRegistration::compute() {}
+}  // namespace PPF
--- PPFRegistration.h
+++ PPFRegistration.h
@@ -13,13 +13,63 @@ namespace PPF {
 
 class PPFRegistration {
  public:
-  PPFRegistration() = default;
+  PPFRegistration(const pcl::PointCloud<pcl::PointNormal>::Ptr &scene,
+                  const pcl::PointCloud<pcl::PointNormal>::Ptr &model,
+                  const Hash::HashMap_<Hash::HashKey, Hash::HashData,
+                                       Hash::hash_cal>::Ptr &search_map,
+                  const Hash::HashMap_<Hash::Trans_key, Hash::Trans_data,
+                                       Hash::Tran_cal>::Ptr &Trans_map)
+      : scene_cloud_with_normal(scene),
+        model_cloud_with_normal(model),
+        searchMap(search_map),
+        model_trans(Trans_map){};
 
-  void compute(const pcl::PointCloud<pcl::PointNormal>::Ptr &scene,
-               const Hash::HashMap_<Hash::HashKey, Hash::HashData,
-                                    Hash::hash_cal>::Ptr &search_map);
+  void compute();
+
+  Eigen::Affine3f getFinalTransformation();
+
+  void setSceneReferencePointSamplingRate(
+      const float &scene_reference_point_sampling_rate);
+
+  void setPositionClusteringThreshold(
+      const float &clustering_position_diff_threshold);
+
+  void setRotationClusteringThreshold(
+      const float &clustering_rotation_diff_threshold);
+
+  void setDiscretizationSteps(const float &angle_discretization_step,
+                              const float &distance_discretization_step);
+
+  void setSearchMap(const Hash::HashMap_<Hash::HashKey, Hash::HashData,
+                                         Hash::hash_cal>::Ptr &searchMap);
+
+  void setTransMap(const Hash::HashMap_<Hash::Trans_key, Hash::Trans_data,
+                                        Hash::Tran_cal>::Ptr &Trans);
+
+  void setInputSource(const pcl::PointCloud<pcl::PointNormal>::Ptr &cloud);
+
+  void setInputTarget(const pcl::PointCloud<pcl::PointNormal>::Ptr &cloud);
+
+  void setDobj(const float &data);
+
+  PPFRegistration &operator=(const PPFRegistration &) = delete;
+  PPFRegistration(const PPFRegistration &) = delete;
 
  private:
+  float scene_reference_point_sampling_rate{};
+  float clustering_position_diff_threshold{};
+  float clustering_rotation_diff_threshold{};
+
+  pcl::PointCloud<pcl::PointNormal>::Ptr model_cloud_with_normal;
+  pcl::PointCloud<pcl::PointNormal>::Ptr scene_cloud_with_normal;
+  Eigen::Affine3f finalTransformation;
+  Hash::HashMap_<Hash::HashKey, Hash::HashData, Hash::hash_cal>::Ptr searchMap;
+  Hash::HashMap_<Hash::Trans_key, Hash::Trans_data, Hash::Tran_cal>::Ptr
+      model_trans;
+
+  float angle_discretization_step;
+  float distance_discretization_step;
+  float d_obj;
 };
 
 }  // namespace PPF
